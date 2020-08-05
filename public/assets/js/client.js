e3df7fea9321e58b6b70bb5776da136b9501ec2f
tql247 <toitenlalinh9xpro@gmail.com>
2020-08-05 18:08:40 +0700

    ok

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,8 +1,9 @@
-var socket = io.connect('http://localhost:8000');
+var socket = io.connect('https://chatrealtimevn.herokuapp.com/');
 
 socket.on('connect', function(date){
     socket.emit('join','A new join');
 });
+
 
 socket.on('thread', function(data){
     $('#thread').append('<li>' + data + '</li> <br>');
